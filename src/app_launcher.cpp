8926ebdfc5fe62e30787cb2af749972902dd9664
ImLunchtime <knoxmedia@yeah.net>
2025-11-20 14:57:00 +0800

    启动器网格布局

--- src/app_launcher.cpp
+++ src/app_launcher.cpp
@@ -2,7 +2,7 @@
  * @Author: ImLunchtime knoxmedia@yeah.net
  * @Date: 2025-11-08 18:13:39
  * @LastEditors: ImLunchtime knoxmedia@yeah.net
- * @LastEditTime: 2025-11-19 14:13:34
+ * @LastEditTime: 2025-11-20 14:52:02
  * @FilePath: \CardputerOS2_LVGL\src\app_launcher.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -57,47 +57,78 @@ void AppLauncher::onOpen(lv_obj_t* window_root) {
     lv_obj_set_style_pad_row(root_, 0, 0);
     lv_obj_set_scroll_dir(root_, LV_DIR_NONE);
 
-    list_ = lv_list_create(root_);
-    lv_obj_set_size(list_, lv_pct(100), lv_pct(100));
-    lv_obj_set_flex_grow(list_, 1);
-    lv_obj_set_pos(list_, 0, 0);
-    lv_obj_set_style_pad_all(list_, 0, 0);
-    lv_obj_set_style_radius(list_, 0, 0);
-    lv_obj_set_style_border_width(list_, 0, 0);
-    lv_obj_set_style_border_opa(list_, LV_OPA_TRANSP, 0);
-    ui_theme::apply_list_menu(list_);
+    grid_ = lv_obj_create(root_);
+    lv_obj_set_size(grid_, lv_pct(100), lv_pct(100));
+    lv_obj_set_flex_grow(grid_, 1);
+    lv_obj_set_style_pad_all(grid_, 0, 0);
+    lv_obj_set_style_pad_row(grid_, 0, 0);
+    lv_obj_set_style_pad_column(grid_, 0, 0);
+    lv_obj_set_style_border_width(grid_, 0, 0);
+    lv_obj_set_style_radius(grid_, 0, 0);
+    lv_obj_set_style_bg_opa(grid_, LV_OPA_TRANSP, 0);
+    static lv_coord_t col_dsc[] = {48, 48, 48, 48, 48, 48, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc[] = {48, LV_GRID_TEMPLATE_LAST};
+    lv_obj_set_grid_dsc_array(grid_, col_dsc, row_dsc);
 
-    lv_obj_t* item_music = lv_list_add_btn(list_, &icon_music_sd, "Music");
-    ui_theme::apply_list_menu_item(item_music);
-    lv_obj_set_style_min_height(item_music, 20, 0);
-    lv_obj_add_event_cb(item_music, on_music_item_event, LV_EVENT_CLICKED, this);
+    lv_obj_t* btn_music = lv_btn_create(grid_);
+    lv_obj_set_size(btn_music, 48, 48);
+    lv_obj_set_style_bg_opa(btn_music, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_border_width(btn_music, 0, 0);
+    lv_obj_set_style_radius(btn_music, 0, 0);
+    lv_obj_set_style_shadow_width(btn_music, 0, 0);
+    lv_obj_set_style_outline_width(btn_music, 0, 0);
+    lv_obj_set_grid_cell(btn_music, LV_GRID_ALIGN_CENTER, 0, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+    lv_obj_add_event_cb(btn_music, on_music_item_event, LV_EVENT_CLICKED, this);
     {
-        lv_obj_t* img = find_img_child(item_music);
-        //if (img) lv_img_set_zoom(img, 128);
+        lv_obj_t* img = lv_img_create(btn_music);
+        lv_img_set_src(img, &icon_music_sd);
+        lv_obj_center(img);
     }
 
-    lv_obj_t* item_theme = lv_list_add_btn(list_, &icon_theme, "Theme Center");
-    ui_theme::apply_list_menu_item(item_theme);
-    lv_obj_set_style_min_height(item_theme, 20, 0);
-    lv_obj_add_event_cb(item_theme, on_theme_center_item_event, LV_EVENT_CLICKED, this);
+    lv_obj_t* btn_theme = lv_btn_create(grid_);
+    lv_obj_set_size(btn_theme, 48, 48);
+    lv_obj_set_style_bg_opa(btn_theme, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_border_width(btn_theme, 0, 0);
+    lv_obj_set_style_radius(btn_theme, 0, 0);
+    lv_obj_set_style_shadow_width(btn_theme, 0, 0);
+    lv_obj_set_style_outline_width(btn_theme, 0, 0);
+    lv_obj_set_grid_cell(btn_theme, LV_GRID_ALIGN_CENTER, 1, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+    lv_obj_add_event_cb(btn_theme, on_theme_center_item_event, LV_EVENT_CLICKED, this);
     {
-        lv_obj_t* img = find_img_child(item_theme);
-        //if (img) lv_img_set_zoom(img, 128);
+        lv_obj_t* img = lv_img_create(btn_theme);
+        lv_img_set_src(img, &icon_theme);
+        lv_obj_center(img);
     }
 
-    lv_obj_t* item_test = lv_list_add_btn(list_, &icon_test, "Test");
-    ui_theme::apply_list_menu_item(item_test);
-    lv_obj_set_style_min_height(item_test, 20, 0);
-    lv_obj_add_event_cb(item_test, on_test_item_event, LV_EVENT_CLICKED, this);
+    lv_obj_t* btn_test = lv_btn_create(grid_);
+    lv_obj_set_size(btn_test, 48, 48);
+    lv_obj_set_style_bg_opa(btn_test, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_border_width(btn_test, 0, 0);
+    lv_obj_set_style_radius(btn_test, 0, 0);
+    lv_obj_set_style_shadow_width(btn_test, 0, 0);
+    lv_obj_set_style_outline_width(btn_test, 0, 0);
+    lv_obj_set_grid_cell(btn_test, LV_GRID_ALIGN_CENTER, 2, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+    lv_obj_add_event_cb(btn_test, on_test_item_event, LV_EVENT_CLICKED, this);
     {
-        lv_obj_t* img = find_img_child(item_test);
-        //if (img) lv_img_set_zoom(img, 128);
+        lv_obj_t* img = lv_img_create(btn_test);
+        lv_img_set_src(img, &icon_test);
+        lv_obj_center(img);
     }
 
-    lv_obj_t* item_settings = lv_list_add_btn(list_, LV_SYMBOL_SETTINGS, "Settings");
-    ui_theme::apply_list_menu_item(item_settings);
-    lv_obj_set_style_min_height(item_settings, 20, 0);
-    lv_obj_add_event_cb(item_settings, on_settings_item_event, LV_EVENT_CLICKED, this);
+    lv_obj_t* btn_settings = lv_btn_create(grid_);
+    lv_obj_set_size(btn_settings, 48, 48);
+    lv_obj_set_style_bg_opa(btn_settings, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_border_width(btn_settings, 0, 0);
+    lv_obj_set_style_radius(btn_settings, 0, 0);
+    lv_obj_set_style_shadow_width(btn_settings, 0, 0);
+    lv_obj_set_style_outline_width(btn_settings, 0, 0);
+    lv_obj_set_grid_cell(btn_settings, LV_GRID_ALIGN_CENTER, 3, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+    lv_obj_add_event_cb(btn_settings, on_settings_item_event, LV_EVENT_CLICKED, this);
+    {
+        lv_obj_t* label = lv_label_create(btn_settings);
+        lv_label_set_text(label, LV_SYMBOL_SETTINGS);
+        lv_obj_center(label);
+    }
 
 }
 
--- src/app_launcher.h
+++ src/app_launcher.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: ImLunchtime knoxmedia@yeah.net
+ * @Date: 2025-11-08 18:13:39
+ * @LastEditors: ImLunchtime knoxmedia@yeah.net
+ * @LastEditTime: 2025-11-20 14:45:18
+ * @FilePath: \CardputerOS2_LVGL\src\app_launcher.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once
 #include "window_system.h"
 
@@ -22,5 +30,5 @@ public:
 private:
     WindowSystem& wm_;
     lv_obj_t* root_ = nullptr;
-    lv_obj_t* list_ = nullptr;
+    lv_obj_t* grid_ = nullptr;
 };
