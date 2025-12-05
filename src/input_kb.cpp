ee12d2a5aefe17de5a50ce7b3dbd1beac34c16e7
ImLunchtime <knoxmedia@yeah.net>
2025-12-05 10:34:21 +0800

    LVGL键盘输入逻辑优化

--- src/input_kb.cpp
+++ src/input_kb.cpp
@@ -2,7 +2,7 @@
  * @Author: ImLunchtime knoxmedia@yeah.net
  * @Date: 2025-11-07 17:33:17
  * @LastEditors: ImLunchtime knoxmedia@yeah.net
- * @LastEditTime: 2025-12-05 10:34:02
+ * @LastEditTime: 2025-12-05 10:34:20
  * @FilePath: \CardputerOS2_LVGL\src\input_kb.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
